c20adf2db095dbe8a5057db3fba043da5fad6740
zlsun <zlsun1995@gmail.com>
2016-04-05 19:50:11 +0800

    Update

--- config/ranger/commands.py
+++ config/ranger/commands.py
@@ -6,25 +6,6 @@ from ranger.api.commands import *
 from ranger.core.loader import CommandLoader
 
 
-class empty(Command):
-    """:empty
-
-    Empties the trash directory ~/.Trash
-    """
-
-    def execute(self):
-        folder = os.path.expanduser('~/.Trash/')
-        for the_file in os.listdir(folder):
-            file_path = os.path.join(folder, the_file)
-            try:
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)
-            except Exception as e:
-                print(e)
-
-
 class extracthere(Command):
 
     def execute(self):
